5b2dff794a621837feedaac5f5a1e5c20ec9ee6e
ibrahimkuralozkan <ibrahimkuralozkan@gmail.com>
2021-11-15 12:07:56 +0100

    openBrowser

--- src/test/java/com/test/tests/FirstClass.java
+++ src/test/java/com/test/tests/FirstClass.java
@@ -1,4 +1,18 @@
 package com.test.tests;
 
+import com.github.javafaker.CreditCardType;
+import com.github.javafaker.Faker;
+
 public class FirstClass {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        Faker faker = new Faker();
+        System.out.println(faker.animal().name());
+        System.out.println(faker.finance().creditCard(CreditCardType.AMERICAN_EXPRESS));
+
+    }
+
 }
